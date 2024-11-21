7506634698cd447bdee82f75e5d98e393bc6adef
DI GREGORIO Nicolas <nicolas.digregorio@gmail.com>
2024-11-21 06:54:42 +0000

    Updated Package squid to version: <strong>6.9-r0</strong>

--- .github/scripts/update.sh
+++ .github/scripts/update.sh
@@ -2,10 +2,22 @@
 
 PACKAGE_NAME=squid; 
 PACKAGE_ARCH=x86_64; 
-ALPINE_VERSION=$(curl -SsL https://raw.githubusercontent.com/digrouz/docker-alpine-s6/master/Dockerfile_alpine | grep "FROM alpine:" | awk '{print $2}'| sed  -E "s|alpine:||" | awk -F '.' '{print $1"."$2}')
+ALPINE_VERSION=$(curl -SsL https://raw.githubusercontent.com/digrouz/docker-alpine-s6/master/Dockerfile_alpine |\
+                 grep "FROM alpine:" |\
+                 awk '{print $2}'|\
+                 sed  -E "s|alpine:||" |\
+                 awk -F '.' '{print $1"."$2}'\
+                )
 
-NEW_VERSION=$(curl -SsL https://pkgs.alpinelinux.org/package/v${ALPINE_VERSION}/main/${PACKAGE_ARCH}/${PACKAGE_NAME} | grep -i -A 3 Version | tail -1 | awk '{print $1}')
-sed -i -e "s|ARG ${PACKAGE_NAME^^}_VERSION=.*|ARG ${PACKAGE_NAME^^}_VERSION='${NEW_VERSION}'|" Dockerfile_alpine
+NEW_VERSION=$(curl -SsL https://pkgs.alpinelinux.org/package/v${ALPINE_VERSION}/main/${PACKAGE_ARCH}/${PACKAGE_NAME} |\
+              grep -i -A 3 Version |\
+              tail -1 | \
+              awk '{print $1}'\
+             )
+
+if [ "${NEW_VERSION}" ]; then
+  sed -i -e "s|ARG ${PACKAGE_NAME^^}_VERSION=.*|ARG ${PACKAGE_NAME^^}_VERSION='${NEW_VERSION}'|" Dockerfile_alpine
+fi
 
 if output=$(git status --porcelain) && [ -z "$output" ]; then
   # Working directory clean
